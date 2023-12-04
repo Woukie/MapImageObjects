efc51f2c50e509877587585d6bfdf0f4dcb74fb5
Lucas Eldon <lucas.eldon.2021@live.rhul.ac.uk>
2023-12-04 08:44:55 +0000

    Added temp project

---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace MapImageObjects
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "MapImageObjects";
+        public const string PLUGIN_NAME = "MapImageObjects";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
