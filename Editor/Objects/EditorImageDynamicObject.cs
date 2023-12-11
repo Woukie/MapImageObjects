5d6096555e4f14d90f23903c717883578192dc2c
Woukie <lucaseldon@live.com>
2023-12-11 18:06:20 +0000

    Added dynamic image

---
+++ Editor/Objects/EditorImageDynamicObject.cs
@@ -0,0 +1,15 @@
+﻿using MapImageObjects.Core;
+using MapsExt.Editor.MapObjects;
+using UnityEngine;
+
+namespace MapImageObjects.Editor.Objects;
+
+[EditorMapObject(typeof(ImageDynamicObjectData), "Image", Category = "Dynamic")]
+public class EditorImageDynamicObject : ImageDynamicObject
+{
+    public override void OnInstantiate(GameObject instance)
+    {
+        instance.GetComponent<Rigidbody2D>().isKinematic = true;
+        base.OnInstantiate(instance);
+    }
+}
